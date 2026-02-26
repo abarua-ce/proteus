8af252b7a427df345f0cd9b70e2ea3ff4a536e6d
abarua-ce <arnobtuku@gmail.com>
2026-02-26 00:14:42 -0600

    Added for Bioswale contamination test parallel

---
+++ test/salinity/Bioswale_contamination/tranport_p.py
@@ -0,0 +1,130 @@
+from __future__ import absolute_import
+from builtins import object
+import numpy as np
+from proteus import *
+from proteus.default_p import *
+from proteus.ctransportCoefficients import smoothedHeaviside
+
+from proteus import Domain
+from proteus import Norms
+from proteus import Profiling
+from proteus import Context
+from proteus.mprans import TADR
+
+from proteus.Profiling import logEvent
+
+from math import *
+from domain_rg import *
+import numpy as np
+
+################################## Inputs ########################################
+
+physicalDiffusion = 0.0 #1.0e-8 # m2/s, molecular diffusion coefficient
+refinement = 0
+
+# SHOCK CAPTURING PARAMETERS #
+shockCapturingFactor_tadr=0.2
+lag_shockCapturing_tadr=True
+
+# General parameters #
+parallel = False # if True use PETSc solvers
+linearSmoother = None
+checkMass = False
+
+
+class MyCoefficients(TADR.Coefficients):
+    def attachModels(self, modelList):
+        """
+        Attach the Richards model to TADR and pass the velocity field (grad(u)).
+        """
+        self.model = modelList[self.modelIndex]
+        # Attach Richards model
+        logEvent(f"[TADR.attachModels] Bound TADR model at idx={self.modelIndex}, "
+                 f"type={type(self.model).__name__}, id={id(self.model)}")
+        logEvent(f"[TADR.attachModels] Coeff==model.coeff? {self.model.coefficients is self}")
+
+        flowModel = modelList[0]  # Richards model
+        
+        for i, model in enumerate(modelList):
+            logEvent(f"Model {i}: {model.name}, Type: {type(model)}")
+        self.q_v = flowModel.q['velocity_couple',0]
+        self.ebqe_v = flowModel.ebqe['velocity_couple',0]
+        # Log for debugging
+        logEvent(f"Richards velocity (q_v): mean={self.q_v.mean()}, min={self.q_v.min()}, max={self.q_v.max()}")
+        logEvent(f"Richards boundary velocity (ebqe_v): mean={self.ebqe_v.mean()}, min={self.ebqe_v.min()}, max={self.ebqe_v.max()}")
+        logEvent("Attached Richards model to TADR coefficients.")
+
+
+LevelModelType = TADR.LevelModel
+logEvent = Profiling.logEvent
+#name=soname
+a0= 18.8571e-6 #e-6
+def a(x):
+    return np.array([[a0,0.0],[0.0,a0]])
+aOfX = {0:a}
+alpha_L= 0.1
+alpha_T= 0.1*alpha_L
+Dm= 18.86e-2 #m2/s, dispersion coefficient
+coefficients = MyCoefficients(
+    alpha_L=alpha_L,
+    alpha_T=alpha_T,
+    Dm=Dm,
+    rho_fw= 1.0,
+    rho_sw = 1.0,
+    V_model = 0,
+    checkMass=checkMass,
+    FCT=True,
+    LUMPED_MASS_MATRIX=True,
+    STABILIZATION_TYPE=2,
+    diagonal_conductivity= True,
+    ENTROPY_TYPE='LOG',
+    cE=0.1, cK=1.0, physicalDiffusion=0.0) #ct.physicalDiffusion)
+coefficients.variableNames=['u']
+
+#####################
+# INITIAL CONDITION #
+#####################
+###########################
+# Defining Initial Conditions Functions
+# Initially will start at all freshwater
+# and then go to a steady state
+###########################
+
+
+cin= 3.00
+def getRGConcDirichletBCs(x,tag):
+    if x[1] == L[1]:
+        if (x[0] >= L[0]/3.0 and
+            x[0] <= 2.0*L[0]/3.0):
+            return lambda x,t: cin
+    else:
+        return None
+
+dirichletConditions = {0:getRGConcDirichletBCs}
+
+class constantIC:
+    def __init__(self,cval=0.0):
+        self.cval = cval
+    def uOfXT(self,x,t):
+        bc= getRGConcDirichletBCs(x,0)
+        if bc != None:
+            return bc(x,t)
+        else:
+            return self.cval
+    def uOfX(self,x):
+        bc= getRGConcDirichletBCs(x,0)
+        if bc != None:
+            return bc(x,t)
+        else:
+            return self.cval
+
+initialConditions ={0:constantIC(0.0)}
+
+def getZeroDiffusiveFlux(x, flag):
+    if flag == boundaryTags['top'] and (L[0]/3.0 <= x[0] <= 2.0*L[0]/3.0):
+        return None  # Dirichlet already specified on the ponded strip
+    return lambda x, t: 0.0
+fluxBoundaryConditions = {0:'setFlow'}
+
+advectiveFluxBoundaryConditions = {0: getZeroDiffusiveFlux}  # use 'setFlow' behavior
+diffusiveFluxBoundaryConditions  = {0: {0: getZeroDiffusiveFlux}}
